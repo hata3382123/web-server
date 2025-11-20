47af7cfe16480cfd61b3fce2c62dfda57dedc7b3
hata3382123 <67373718+hata3382123@users.noreply.github.com>
2025-11-20 20:01:47 +0800

    新增中间件解决跨域问题

--- webook/internal/web/user.go
+++ webook/internal/web/user.go
@@ -1,10 +1,30 @@
 package web
 
 import (
+	"net/http"
+
+	regexp "github.com/dlclark/regexp2"
+
 	"github.com/gin-gonic/gin"
 )
 
-type UserHandler struct{}
+type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegexPattern    = `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+		passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_\-+=]{6,8}$`
+	)
+	emailExp := regexp.MustCompile(emailRegexPattern, regexp.None)
+	passwordExp := regexp.MustCompile(passwordRegexPattern, regexp.None)
+	return &UserHandler{
+		emailExp:    emailExp,
+		passwordExp: passwordExp,
+	}
+}
 
 func (u UserHandler) RegisterRoutes(server *gin.Engine) {
 	ug := server.Group("/users")
@@ -15,7 +35,48 @@ func (u UserHandler) RegisterRoutes(server *gin.Engine) {
 	ug.GET("/profile", u.Profile)
 }
 
-func (u UserHandler) SignUp(c *gin.Context)  {}
+func (u UserHandler) SignUp(c *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+	}
+	var req SignUpReq
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": 1, "msg": "请求参数错误: " + err.Error()})
+		return
+	}
+
+	//邮箱校验
+	ok, err := u.emailExp.MatchString(req.Email)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"code": 4, "msg": "系统错误"})
+		return
+	}
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{"msg": "你的邮箱格式不正确"})
+		return
+	}
+
+	//密码校验
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"code": 4, "msg": "系统错误"})
+		return
+	}
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{"msg": "你的密码格式不正确"})
+		return
+	}
+	//两次密码校验
+	if req.ConfirmPassword != req.Password {
+		c.JSON(http.StatusOK, gin.H{"msg": "两次输入的密码不一致"})
+		return
+	}
+	c.String(http.StatusOK, "signup success")
+	//数据库操作
+
+}
 func (u UserHandler) Login(c *gin.Context)   {}
 func (u UserHandler) Edit(c *gin.Context)    {}
 func (u UserHandler) Profile(c *gin.Context) {}
--- webook/main.go
+++ webook/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"strings"
+	"time"
 	"webook/internal/web"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
-	u := &web.UserHandler{}
+	//use 作用于全部路由
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins:     []string{"https://foo.com"},
+		//AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders: []string{"authorization", "content-type"},
+		//ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "mycompany.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
 	server.Run(":8080")
 }
